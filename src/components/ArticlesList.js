f6f9f050a5b6a9af9d8a8a835b3de23d590edb04
Adalberto <adalbertoperdomoabreu@gmail.com>
2022-03-20 17:05:26 +0000

    Sorter  hecho.

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -5,6 +5,7 @@ import Article from './Article';
 
 
 export default function ArticlesList(props) {
+    const [activeSorter, setActiveSorter] = useState("recientes");
     const [actualPage, setActualPage] = useState(1);
     const TOTAL_BY_PAGES = 4;
     const activeCategories = props.activeCategories;
@@ -32,6 +33,7 @@ export default function ArticlesList(props) {
 
     const dataLoad = () => {
         data = filterData(data);
+        data = sortDataBySelect();
         data = data.slice(
             (actualPage - 1) * TOTAL_BY_PAGES,
             actualPage * TOTAL_BY_PAGES
@@ -52,6 +54,50 @@ export default function ArticlesList(props) {
         window.location.href = "#articles-list";
     }
 
+    const changeActiveSorter = () => {
+        let sorter = document.getElementById("sorter");
+        let options = ["recientes", "antiguos", "mejores"];
+        let optionSelected = options.indexOf(sorter.value);
+
+        setActiveSorter(options[optionSelected]);
+    }
+
+    const sortDataBySelect = () => {
+        return data.sort((a, b) => {
+            if (activeSorter === "recientes"
+                || activeSorter === "antiguos") {
+                return sortByDate(a, b);
+            }
+            return sortByScore(a, b);
+        });
+    }
+
+    const sortByDate = (a, b) => {
+        const aDateArray = a.date.split("/");
+        const bDateArray = b.date.split("/");
+
+        if (aDateArray === null && bDateArray === null) return 0;
+
+        for (let i = 2; i >= 0; i--) {
+            if (aDateArray[i] === bDateArray[i]) {
+                continue;
+            }
+            return (
+                aDateArray[i].toString().localeCompare(bDateArray[i].toString(), "en", {
+                    numeric: true,
+                }) * (activeSorter === "antiguos" ? 1 : -1)
+            );
+        }
+    }
+
+    const sortByScore = (a, b) => {
+        return (
+            a.valoration.toString().localeCompare(b.valoration.toString(), "en", {
+                numeric: true,
+            }) * (activeSorter === "antiguos" ? 1 : -1)
+        );
+    }
+
     dataLoad();
 
     useEffect(() => {
@@ -64,8 +110,8 @@ export default function ArticlesList(props) {
         <section className='articles-list' id='articles-list'>
             <div className='articles-header'>
                 <h2>{props.title}</h2>
-                <select id='sorter'>
-                    <option value="recientes">Recientes</option>
+                <select id='sorter' onChange={changeActiveSorter}>
+                    <option value="recientes" selected>Recientes</option>
                     <option value="antiguos">Más Antiguos</option>
                     <option value="mejores">Mejores</option>
                 </select>
